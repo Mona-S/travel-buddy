5bd2098e9baf439b45f2ac48325945d6f49da682
Mona <forward2cm@gmail.com>
2019-08-01 11:22:57 -0700

    added geocode and marker

--- googlemaps.js
+++ googlemaps.js
@@ -1,70 +1,89 @@
 class GoogleMaps{
-  constructor(lat, lng){
-    this.lat = lat;
-    this.lng = lng;
+  constructor(address){
+  // this.lat = lat;
+  // this.lng = lng;
     this.map = map;
+    this.address = address;
     this.initMap = this.initMap.bind(this);
     this.addEventHandler = this.addEventHandler.bind(this);
     this.handleMapClick = this.handleMapClick.bind(this);
+    this.clearMarker = this.clearMarker.bind(this);
+    this.marker = null;
+    this.addMarker = this.addMarker.bind(this);
+  
   }
 
   initMap(){
-    var myLatLong = {lat: this.lat, lng: this.lng};
-    this.map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: this.lat, lng: this.lng},
-    zoom: 8});
+    //var myLatLong = {lat: this.lat, lng: this.lng};
+    var geocoder = new google.maps.Geocoder()
+    geocoder.geocode({'address': this.address}, function(results, status) {
+      if (status === 'OK') {
+        var position = results[0].geometry.location;
+        this.lat = position.lat();
+        this.lng  = position.lng(); 
+        console.log('new', this.lat , this.lng);
+        this.map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: this.lat , lng: this.lng},
+          zoom: 10});
 
-
+        this.addMarker();
+        
+       
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    }.bind(this));
+    
   }
 
+ 
   addMarker(){
+    if(this.marker){this.setMapOnAll(null)};
     const marker = new google.maps.Marker({
-    position: {lat: this.lat, lng: this.lng},
+        position: {lat: this.lat, lng: this.lng},
     map: this.map,
     title: 'Click to Zoom'
+  }); 
+    this.marker = marker;
+    console.log('here' , this.lat, this.lng);
+  }
 
-  });
-}
+  setMapOnAll(map) {
+      this.marker.setMap(map);
+  }
 
   addEventHandler(){
     google.maps.event.addListener(this.map, 'click', this.handleMapClick);
+    
+
   }
+
+    handleMarkerClick(){
+      map.setZoom(8);
+      map.setCenter(this.marker.getposition());
+    
+  }
+    
   handleMapClick(event){
     this.lat = event.latLng.lat();
     this.lng = event.latLng.lng();
     console.log(this.lat, this.lng);
+    this.addMarker();
+    
   }
+
+  clearMarker(){
+      setMapOnAll(null);
+  }
+
+  getLatLng(){
+    return [this.lat, this.lng];
+
+  }
+
+    
 }
 
-
-
-
-
-
-// let lat = null;
-// let lng = null;
-
-// function initMap() {
-//     const myLatLong = {lat: -25.363, lng: 131.044};
-//     const map = new google.maps.Map(document.getElementById('map'), {
-//       center: {lat: -34.397, lng: 150.644},
-//       zoom: 8
-//     });
-
-//     const marker = new google.maps.Marker({
-//       position: myLatLong,
-//       map: map,
-//       title: 'Click to Zoom'
-//     });
-    
-//     google.maps.event.addListener(map, 'click', function(event){ 
-//         lat = event.latLng.lat();
-//         lng = event.latLng.lng();
-//         console.log(lat, lng);
-       
-//     });  
-
-    
-// }
+ 
 
  
--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 $(document).ready(initializeApp);
 
-// let gmap;
+let gmap;
 function initializeApp(){
-    const gmap = new GoogleMaps(33.6 , -117);
+    // Click handler
+    //var location = document.getElementById('search').value;
+   // const gmap = new GoogleMaps(33.6 , -117);
+   const gmap = new GoogleMaps('Irvine');
     gmap.initMap();
+   
+
+    //const gloc = new Geocode('Irvine');
+    //gloc.geocodeAddress();
     gmap.addEventHandler();
     const yelp = new Yelp(33.6 , -117);
     yelp.getYelpData();
