db40b4beda20b3b1f66ea85cdaeffd81159e51e1
James Yamagata <wilsonhj@uci.edu>
2019-08-01 16:11:08 -0700

    minor updates

--- main.js
+++ main.js
@@ -2,25 +2,17 @@ $(document).ready(initializeApp);
 
 
 function initializeApp(){
-
     let gmap = new GoogleMaps('Irvine');
     gmap.initMap();
-
-   
     const yelp = new Yelp(33.6 , -117, "Irvine");
     yelp.getYelpData();
     const weather = new Weather;
     const flickr = new Flickr;
     
     $("#flickrAndWeather").on("click", function(){
-        weather.cityInput = $("#cityInput").val();
-        flickr.cityInput = $("#cityInput").val();
-
+      yelp.location = flickr.cityInput  = weather.cityInput = $("#cityInput").val();
         gmap = new GoogleMaps($("#cityInput").val());
         gmap.initMap();
-
-        yelp.location = $("#cityInput").val(); 
-
         weather.getWeather();
         flickr.getFlickr();
         yelp.getLocationData();
