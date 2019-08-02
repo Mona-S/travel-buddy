e327aab8e010376fbad9d3ff5ef9a569975bfdc8
Jaehyuk Huh <jaehuh86@yahoo.com:git config --global user.name Jae>
2019-08-01 17:10:30 -0700

    cleaned up code & minor css

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 $(document).ready(initializeApp);
 
-
 function initializeApp(){
 
     let gmap = new GoogleMaps('Irvine');
     gmap.initMap();
 
-   
     const yelp = new Yelp(33.6 , -117, "Irvine");
     yelp.getYelpData();
     const weather = new Weather;
     const flickr = new Flickr;
-    
+
     $("#flickrAndWeather").on("click", function(){
         weather.cityInput = $("#cityInput").val();
         flickr.cityInput = $("#cityInput").val();
@@ -19,7 +17,7 @@ function initializeApp(){
         gmap = new GoogleMaps($("#cityInput").val());
         gmap.initMap();
 
-        yelp.location = $("#cityInput").val(); 
+        yelp.location = $("#cityInput").val();
 
         weather.getWeather();
         flickr.getFlickr();
